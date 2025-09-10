87fc561acae6ae26e819cd18e6a39177e49eb5a5
kec <wlsqkd1199@naver.com>
2025-09-10 12:33:35 +0900

    dd
    dd

--- semi/src/main/java/com/kh/semi/schedule/controller/ScheduleController.java
+++ semi/src/main/java/com/kh/semi/schedule/controller/ScheduleController.java
@@ -1,4 +1,4 @@
-package com.kh.semi.schdule.controller;
+package com.kh.semi.schedule.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
